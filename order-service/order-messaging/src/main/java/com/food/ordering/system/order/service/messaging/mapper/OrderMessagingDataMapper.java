ba1eb9e3f2afec4e202360a42adf4f11d0d04cfa
Johan Rodriguez <johan.rodriguez@contractor.brightinsight.com>
2023-07-14 10:43:08 -0500

    Added listeners and main class for order service

--- order-service/order-messaging/src/main/java/com/food/ordering/system/order/service/messaging/mapper/OrderMessagingDataMapper.java
+++ order-service/order-messaging/src/main/java/com/food/ordering/system/order/service/messaging/mapper/OrderMessagingDataMapper.java
@@ -1,8 +1,11 @@
 package com.food.ordering.system.order.service.messaging.mapper;
 
+import com.food.ordering.system.domain.valueobject.OrderApprovalStatus;
+import com.food.ordering.system.domain.valueobject.PaymentStatus;
 import com.food.ordering.system.kafka.order.avro.model.*;
+import com.food.ordering.system.order.service.domain.dto.messages.PaymentResponse;
+import com.food.ordering.system.order.service.domain.dto.messages.RestaurantApprovalResponse;
 import com.food.ordering.system.order.service.domain.entity.Order;
-import com.food.ordering.system.order.service.domain.entity.OrderItem;
 import com.food.ordering.system.order.service.domain.event.OrderCancelledEvent;
 import com.food.ordering.system.order.service.domain.event.OrderCreatedEvent;
 import com.food.ordering.system.order.service.domain.event.OrderPaidEvent;
@@ -50,12 +53,37 @@ public class OrderMessagingDataMapper {
                 .setCreatedAt(orderPaidEvent.getCreatedAt().toInstant())
                 .setRestaurantId(order.getRestaurantId().toString())
                 .setRestaurantOrderStatus(RestaurantOrderStatus.PAID)
-                .setProducts(order.getItems().stream().map(item -> {
-                    return Product.newBuilder()
-                            .setId(item.getProduct().getId().getValue().toString())
-                            .setQuantity(item.getQuantity())
-                            .build();
-                }).collect(Collectors.toList()))
+                .setProducts(order.getItems().stream().map(item -> Product.newBuilder()
+                        .setId(item.getProduct().getId().getValue().toString())
+                        .setQuantity(item.getQuantity())
+                        .build()).collect(Collectors.toList()))
+                .build();
+    }
+
+    public PaymentResponse paymentResponseAvroModelToPaymentCompleted(PaymentResponseAvroModel paymentResponseAvroModel) {
+        return PaymentResponse.builder()
+                .id(paymentResponseAvroModel.getId())
+                .orderId(paymentResponseAvroModel.getOrderId())
+                .paymentId(paymentResponseAvroModel.getPaymentId())
+                .price(paymentResponseAvroModel.getPrice())
+                .paymentStatus(PaymentStatus.valueOf(paymentResponseAvroModel.getPaymentStatus().name()))
+                .sagaId(paymentResponseAvroModel.getSagaId())
+                .customerId(paymentResponseAvroModel.getCustomerId())
+                .createdAt(paymentResponseAvroModel.getCreatedAt())
+                .failureMessages(paymentResponseAvroModel.getFailureMessages())
+                .build();
+    }
+
+    public RestaurantApprovalResponse restaurantApprovalResponseAvroModelToRestaurantApprovalResponse
+            (RestaurantApprovalResponseAvroModel restaurantApprovalResponseAvroModel) {
+        return RestaurantApprovalResponse.builder()
+                .id(restaurantApprovalResponseAvroModel.getId())
+                .restaurantId(restaurantApprovalResponseAvroModel.getRestaurantId())
+                .orderId(restaurantApprovalResponseAvroModel.getOrderId())
+                .orderApprovalStatus(OrderApprovalStatus.valueOf(restaurantApprovalResponseAvroModel.getOrderApprovalStatus().name()))
+                .createdAt(restaurantApprovalResponseAvroModel.getCreatedAt())
+                .sagaId(restaurantApprovalResponseAvroModel.getSagaId())
+                .failureMessages(restaurantApprovalResponseAvroModel.getFailureMessages())
                 .build();
     }
 }
