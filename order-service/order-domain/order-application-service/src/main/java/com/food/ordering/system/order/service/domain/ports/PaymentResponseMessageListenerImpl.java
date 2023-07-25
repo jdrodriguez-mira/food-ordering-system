a1789248669e0c222dc4cbd0bcd5a3d2311809d1
Johan Rodriguez <johan.rodriguez@contractor.brightinsight.com>
2023-07-25 17:13:13 -0500

    Completed SAGA pattern

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/PaymentResponseMessageListenerImpl.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/PaymentResponseMessageListenerImpl.java
@@ -1,7 +1,9 @@
 package com.food.ordering.system.order.service.domain.ports;
 
 import com.food.ordering.system.order.service.domain.dto.messages.PaymentResponse;
+import com.food.ordering.system.order.service.domain.event.OrderPaidEvent;
 import com.food.ordering.system.order.service.domain.ports.input.message.listener.payment.PaymentResponseMessageListener;
+import com.food.ordering.system.order.service.domain.ports.output.message.publisher.restaurantapproval.OrderPaidRestaurantRequestMessagePublisher;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
@@ -11,13 +13,27 @@ import org.springframework.validation.annotation.Validated;
 @Component
 public class PaymentResponseMessageListenerImpl implements PaymentResponseMessageListener {
 
+    private final OrderPaymentSaga orderPaymentSaga;
+    private final OrderPaidRestaurantRequestMessagePublisher orderPaidRestaurantRequestMessagePublisher;
+
+    public PaymentResponseMessageListenerImpl(OrderPaymentSaga orderPaymentSaga,
+                                              OrderPaidRestaurantRequestMessagePublisher orderPaidRestaurantRequestMessagePublisher) {
+        this.orderPaymentSaga = orderPaymentSaga;
+        this.orderPaidRestaurantRequestMessagePublisher = orderPaidRestaurantRequestMessagePublisher;
+    }
+
     @Override
     public void paymentCompleted(PaymentResponse paymentResponse) {
-
+        OrderPaidEvent orderPaidEvent = orderPaymentSaga.process(paymentResponse);
+        log.info("Publishing orderPaidEvent for order id {}", paymentResponse.getOrderId());
+        orderPaidRestaurantRequestMessagePublisher.publish(orderPaidEvent);
     }
 
     @Override
     public void paymentCancelled(PaymentResponse paymentResponse) {
-
+        orderPaymentSaga.rollback(paymentResponse);
+        log.info("Order is roll backed for order id: {} with failure messages: {}",
+                paymentResponse.getOrderId(),
+                String.join(",", paymentResponse.getFailureMessages()));
     }
 }
--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/RestaurantApprovalResponseMessageListenerImpl.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/RestaurantApprovalResponseMessageListenerImpl.java
@@ -1,7 +1,9 @@
 package com.food.ordering.system.order.service.domain.ports;
 
 import com.food.ordering.system.order.service.domain.dto.messages.RestaurantApprovalResponse;
+import com.food.ordering.system.order.service.domain.event.OrderCancelledEvent;
 import com.food.ordering.system.order.service.domain.ports.input.message.listener.restaurantapproval.RestaurantApprovalResponseMessageListener;
+import com.food.ordering.system.order.service.domain.ports.output.message.publisher.payment.OrderCancelledPaymentRequestMessagePublisher;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
@@ -11,13 +13,26 @@ import org.springframework.validation.annotation.Validated;
 @Component
 public class RestaurantApprovalResponseMessageListenerImpl implements RestaurantApprovalResponseMessageListener {
 
+    private final OrderCancelledPaymentRequestMessagePublisher orderCancelledPaymentRequestMessagePublisher;
+    private final OrderApprovalSaga orderApprovalSaga;
+
+    public RestaurantApprovalResponseMessageListenerImpl(OrderCancelledPaymentRequestMessagePublisher orderCancelledPaymentRequestMessagePublisher, OrderApprovalSaga orderApprovalSaga) {
+        this.orderCancelledPaymentRequestMessagePublisher = orderCancelledPaymentRequestMessagePublisher;
+        this.orderApprovalSaga = orderApprovalSaga;
+    }
+
     @Override
     public void orderApproved(RestaurantApprovalResponse restaurantApprovalResponse) {
-
+        orderApprovalSaga.process(restaurantApprovalResponse);
+        log.info("Order is approved for order id: {}", restaurantApprovalResponse.getOrderId());
     }
 
     @Override
     public void orderRejected(RestaurantApprovalResponse restaurantApprovalResponse) {
-
+        OrderCancelledEvent orderCancelledEvent = orderApprovalSaga.rollback(restaurantApprovalResponse);
+        log.info("Publishing order cancelled event for order id: {} with failure messages: {}",
+                restaurantApprovalResponse.getOrderId(),
+                String.join(",", restaurantApprovalResponse.getFailureMessages()));
+        orderCancelledPaymentRequestMessagePublisher.publish(orderCancelledEvent);
     }
 }
